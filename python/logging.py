5b7a23e3391c3eccd7773c9c595bf0c8ef5afb07
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2025-04-04 13:17:31 -0700

    gmem.py no longer needed - superceded by device

--- python/logging.py
+++ python/logging.py
@@ -1,9 +1,9 @@
 import time
 from pixell import memory, colors
-from . import gmem, device
+from . import device
 
 class Logger:
-	def __init__(self, dev=None, level=0, id=0, fmt="{id:3d} {t:6.2f} {mem:6.2f} {gmem_rest:6.2f} {gmem:6.2f} {msg:s}"):
+	def __init__(self, dev=None, level=0, id=0, fmt="{id:3d} {t:6.2f} {mem:6.2f} {dmem_rest:6.2f} {dmem:6.2f} {msg:s}"):
 		"""Memory device memory output won't work if dev isn't passed"""
 		self.level = level
 		self.id    = id
@@ -13,11 +13,11 @@ class Logger:
 	def print(self, message, level=0, id=None, color=None, end="\n"):
 		if level > self.level: return
 		if id is not None and id != self.id: return
-		gmem_tot     = self.dev.memuse(type="total") if self.dev else 0
-		gmem_pools   = self.dev.memuse(type="pools") if self.dev else 0
-		gmem_rest    = self.dev.memuse(type="np")-gmem_pools if self.dev else 0
-		gmem_unknown = gmem_tot - gmem_pools - gmem_rest
-		msg = self.fmt.format(id=self.id, t=(time.time()-self.t0)/60, mem=memory.current()/1024**3, max=memory.max()/1024**3, gmem=gmem_tot/1024**3, gmem_pools=gmem_pools/1024**3, gmem_rest=gmem_rest/1024**3, gmem_unknown=gmem_unknown/1024**3, msg=message)
+		dmem_tot     = self.dev.memuse(type="total") if self.dev else 0
+		dmem_pools   = self.dev.memuse(type="pools") if self.dev else 0
+		dmem_rest    = self.dev.memuse(type="np")-dmem_pools if self.dev else 0
+		dmem_unknown = dmem_tot - dmem_pools - dmem_rest
+		msg = self.fmt.format(id=self.id, t=(time.time()-self.t0)/60, mem=memory.current()/1024**3, max=memory.max()/1024**3, dmem=dmem_tot/1024**3, dmem_pools=dmem_pools/1024**3, dmem_rest=dmem_rest/1024**3, dmem_unknown=dmem_unknown/1024**3, msg=message)
 		if color is not None:
 			msg = color + msg + colors.reset
 		print(msg, end=end)

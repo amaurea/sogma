Fplane movies in sogma. Prot noise model, which was a failure. nopre, fullobs and all-tags in solist. Make soplanet work again. Sign convention in lowpass. Added highpass. Fix missing dev in gapfill in svd_filter. Handle default of sqlite.open being read-only now. More constants in queries. Bore_ref instead of overpole. Added preliminary prior support in mapmaker. Added unweighted version or ra-distribution. Soplanet now uses filters instead of abusing noise models for the same effect. Add sun and moon to list of planets, so we can block them even without the big sidelobe masks. Should have committed more often!

python/gutils.py
@@ -42,16 +42,10 @@ def cumsum0(vals, endpoint=False):
 def detsum_ps(ftod):
 	"""Given complex ftod[:,:], return the equivalent of
 	np.sum(np.abs(ftod)**2,0), but without creating large
-	temporaries"""
-	ap    = device.anypy(ftod)
-	dtype = utils.real_dtype(ftod.dtype)
-	assert ftod.strides[-1] == ftod.dtype.itemsize, "Must be contiguous in last dimension"
-	rtod  = ftod.view(dtype)
-	# I tried doing this in one step with einsum("aic,aic->i"), where c
-	# was a len-2 axis for the real and complex parts, but that was 20x
-	# slower!
-	ps    = ap.einsum("ai,ai->i", rtod, rtod)
-	ps    = ap.sum(ps.reshape(-1,2),-1)
+	temporaries."""
+	ap   = device.anypy(ftod)
+	ps   = ap.linalg.norm(ftod,axis=0)
+	ps **= 2
 	return ps
 
 def detmean_ps(ftod):
@@ -504,7 +498,7 @@ def logint(arr, x):
 
 def obs_group_info(obsinfo, groups, inds=None, sampranges=None):
 	"""Given an obsinfo (ala socommon.finish_query), group definitions
-	groups[ngroup][obsinds], and optionally a lis of which groups to actually include,
+	groups[ngroup][obsinds], and optionally a list of which groups to actually include,
 	returns information about number of detectors and samples per group,
 	per member etc, which would be useful for precallocating buffers."""
 	if inds is None: inds = np.arange(len(groups))
@@ -630,16 +624,18 @@ def select_groups(joint, inds):
 		bands=joint.bands, nullbands=joint.nullbands, joint=joint.joint)
 
 def alloc_rfft(ishape, idtype, axes=[-1], dev=None, pool=None):
-	if dev  is None: dev  = device.get_device()
-	if pool is None: pool = dev.np
+	if pool is None:
+		if dev is None: dev = device.get_device()
+		pool = dev.np
 	ctype  = np.result_type(idtype, 0j)
 	oshape = fft.rfft_shape(ishape, axes=axes)
 	ft     = pool.empty(oshape, ctype)
 	return ft
 
 def alloc_irfft(ishape, idtype,  axes=[-1], n=None, dev=None, pool=None):
-	if dev  is None: dev  = device.get_device()
-	if pool is None: pool = dev.np
+	if pool is None:
+		if dev is None: dev = device.get_device()
+		pool = dev.np
 	rtype  = np.zeros(1, idtype).real.dtype
 	oshape = fft.irfft_shape(ishape, axes=axes, n=n)
 	tod    = pool.empty(oshape, rtype)
@@ -679,7 +675,7 @@ def jacobi(tod, op, iop=lambda x:x, niter=4):
 		res += op(tod-iop(res))
 	return res
 
-def lowpass(tod, fknee, alpha=-8, srate=1, dev=None, pool=None, inplace=False):
+def lowpass(tod, fknee, alpha=8, srate=1, dev=None, pool=None, inplace=False):
 	if dev is None: dev = device.get_device()
 	if not inplace: tod = tod.copy()
 	ft  = alloc_rfft(tod.shape, tod.dtype, dev=dev, pool=pool)
@@ -690,6 +686,9 @@ def lowpass(tod, fknee, alpha=-8, srate=1, dev=None, pool=None, inplace=False):
 	ft *= flt / tod.shape[-1]
 	dev.lib.irfft(ft, tod)
 	return tod
+
+def highpass(tod, fknee, alpha=8, srate=1, dev=None, pool=None, inplace=False):
+	return lowpass(tod, fknee, alpha=-alpha, srate=srate, dev=dev, pool=pool, inplace=inplace)
 
 def svd_filter(tod, eiglim=1e-3, dev=None):
 	if dev is None: dev = device.get_device()
@@ -751,7 +750,7 @@ def estimate_atm_tod(tod, cut=None, srate=400, fmax=10, fmin=0.05, niter=None, e
 	ndet, nsamp = tod.shape
 	# 1. Start with simple gapfilling, so we don't have potentially huge values
 	#    messing things up
-	cut.gapfill(tod)
+	cut.gapfill(tod, dev=dev)
 	# 2. Downsample to target fmax. This not only greatly reduces our
 	#    resource requirements, it also protects us from whtie noise leakage
 	n     = utils.floor(tod.shape[1]*fmax/(srate/2))
@@ -763,7 +762,7 @@ def estimate_atm_tod(tod, cut=None, srate=400, fmax=10, fmin=0.05, niter=None, e
 	for it in range(niter):
 		# Redo gapfilling relative to the best model so far
 		if model is not None:
-			dtod = dcut.gapfill(dtod-model)+model
+			dtod = dcut.gapfill(dtod-model, dev=dev)+model
 		work = dtod.copy()
 		# 3. Factor out super-slow modes, which we may be unrepresentative.
 		model   = lowpass(work, fmin, srate=fmax, dev=dev)
@@ -843,7 +842,7 @@ def read_detnames(fname):
 class LinResamp:
 	"""Helper for resampling arrays using linear interpolation
 	in a way that's compatible with fourier-resampling. Useful for
-	e.g. ctime or boresight (though az must be unwound first"""
+	e.g. ctime or boresight (though az must be unwound first)"""
 	def __init__(self, nin, nout):
 		self.nin, self.nout = nin, nout
 		x  = np.arange(nout)*nin/nout
@@ -856,6 +855,41 @@ class LinResamp:
 		axis= axis % arr.ndim
 		pre = (slice(None),)*axis
 		return arr[pre+(self.x1,)]*self.r1 + arr[pre+(self.x2,)]*self.r2
+
+def find_det_tuples(obs, nmin=0, transpose=False):
+	"""Return a list of the 1-tuples, 2-tuples, ...,
+	where the 1-tuples are unpaired detectors, 2-tuples
+	are paired detectors and the rest are tripled,
+	etc. detectors (not present in SO). The entry for
+	the N-tuples has shape [:,N] if transpose is False
+	and [N,:] if transpose is True, and contains integer
+	indices."""
+	labels  = utils.label_multi([obs.detpix, obs.bands])
+	# Slow implementation
+	groups  = utils.find_equal_groups(labels)
+	tupless = [[] for i in range(nmin)]
+	for gi, group in enumerate(groups):
+		n = len(group)
+		while n > len(tupless):
+			tupless.append([])
+		tupless[n-1].append(group)
+	for i in range(len(tupless)):
+		tupless[i] = np.array(tupless[i],int)
+		if transpose: tupless[i] = tupless[i].T
+	return tupless
+
+def pair_rotate(iptod, optod, normalize=True):
+	"""Given iptod[2,npair,nsamp], construct optod[2,npair,nsamp]
+	such that optod[0] = iptod[0]+iptod[1] and optod[1] = iptod[0]-iptod[1].
+	If normalize is True, then this function is its own inverse.
+	Otherwise, the round trip applies a factor of 2.
+	This function does not allocate."""
+	optod[0]  = iptod[0]
+	optod[0] += iptod[1]
+	optod[1]  = iptod[0]
+	optod[1] -= iptod[1]
+	if normalize: optod /= 2**0.5
+	return optod
 
 def check_demod(demod="auto", has_hwp=False):
 	if demod not in ["auto", "yes", "no"]:
@@ -1013,7 +1047,7 @@ def merge_metadbs(ifnames, ofname, verbose=False):
 	utils.mkdir(os.path.dirname(ofname))
 	utils.rm(ofname)
 	npfile = 0
-	with sqlite.open(ofname) as ofile:
+	with sqlite.open(ofname, mode="rwc") as ofile:
 		for fi, ifname in enumerate(ifnames):
 			if verbose: print(ifname)
 			idir = os.path.dirname(ifname)
@@ -1050,7 +1084,7 @@ def merge_obsdbs(ifnames, ofname, verbose=False):
 	# Prepare to write a new file db from scratch
 	utils.mkdir(os.path.dirname(ofname))
 	utils.rm(ofname)
-	with sqlite.open(ofname) as ofile:
+	with sqlite.open(ofname, mode="rwc") as ofile:
 		for fi, ifname in enumerate(ifnames):
 			if verbose: print(ifname)
 			idir = os.path.dirname(ifname)
@@ -1069,7 +1103,7 @@ def merge_obsfiledbs(ifnames, ofname, verbose=False):
 	# Prepare to write a new file db from scratch
 	utils.mkdir(os.path.dirname(ofname))
 	utils.rm(ofname)
-	with sqlite.open(ofname) as ofile:
+	with sqlite.open(ofname, mode="rwc") as ofile:
 		for fi, ifname in enumerate(ifnames):
 			if verbose: print(ifname)
 			idir = os.path.dirname(ifname)

python/loaders/sofast.py
@@ -143,7 +143,7 @@ class SoFastLoader:
 				meta.aman.restrict("samps", slice(samprange[0]+off,samprange[1]+off), in_place=True)
 		# Set up total obs
 		otot = bunch.Bunch(ctime=None, boresight=None, hwp=None, tod=None, subids=[], errors=[], cuts=[])
-		append_fields = [("dets",0),("detids",0),("bands",0),("point_offset",0),
+		append_fields = [("dets",0),("detids",0),("detpix",0),("bands",0),("point_offset",0),
 			("polangle",0),("response",1)]
 		for field, axis in append_fields: otot[field] = []
 		dcum = 0
@@ -170,7 +170,7 @@ class SoFastLoader:
 				otot.boresight = obs.boresight
 				otot.hwp       = obs.hwp
 				otot.site      = obs.site
-				otot.overpole  = obs.overpole
+				otot.bore_ref  = obs.bore_ref
 				# This is where we finally put things in the "tod" pool. Until now, we've
 				# used the input buffer "itod"
 				otot.tod       = self.dev.pools["tod"].zeros((ndet,len(obs.ctime)), obs.tod.dtype)
@@ -255,6 +255,7 @@ class FastMeta:
 			if detids is not None: good &= np.isin(detinfo.dets,     detids, assume_unique=True)
 			aman = minisotodlib.AxisManager(minisotodlib.LabelAxis("dets", detinfo.channels[good]))
 			aman.wrap("det_ids", detinfo.dets[good], [(0,"dets")])
+			aman.wrap("det_pix", detinfo.pix [good], [(0,"dets")])
 			aman.wrap("bands",   detname2band(detinfo.dets[good]), [(0,"dets")])
 		ndet_full = aman.dets.count
 		# 2. Load the necessary info from det_cal
@@ -442,7 +443,6 @@ def calibrate(data, meta, mul=32, dev=None, prev_obs=None):
 	timestamps, signal, cuts, raw_cuts, jumps, az, el = [a[...,i1:i2] for a in [data.timestamps,data.signal,cuts,raw_cuts,jumps,data.az,data.el]]
 	hwp_angle = meta.aman.hwp_angle[i1:i2] if "hwp_angle" in meta.aman else None
 	ninit = data.dets.count
-	overpole = el[0]>90
 
 	# prev_obs lets us pass in the result of calibrate run on
 	# a different set of detectors for the same observation.
@@ -451,6 +451,7 @@ def calibrate(data, meta, mul=32, dev=None, prev_obs=None):
 	if prev_obs is not None:
 		ctime        = prev_obs.ctime
 		el, az, roll = prev_obs.boresight
+		bore_ref     = prev_obs.bore_ref
 	else:
 		with bench.mark("ctime"):
 			ctime   = timestamps * meta.timestamp_to_ctime
@@ -464,6 +465,7 @@ def calibrate(data, meta, mul=32, dev=None, prev_obs=None):
 			else:
 				# LAT: corotator angle → roll
 				roll = -data.corot[i1:i2]*utils.degree + el - 60*utils.degree
+		bore_ref = np.array([el[0], az[0], roll[0]])
 		with bench.mark("pointing correction"):
 			az, el, roll = apply_pointing_model(az, el, roll, meta.pointing_model)
 
@@ -573,7 +575,6 @@ def calibrate(data, meta, mul=32, dev=None, prev_obs=None):
 		if len(cuts.bins) == 0: raise utils.DataMissing("no detectors left after sanity cuts: raw %d meta %d rms %d cutdens %d overcut %d" % (meta.ndet_full, meta.aman.dets.count, nrms, ndens, nfinal))
 
 	# Sogma uses the cut format [{dets,starts,lens},:]. Translate to this
-
 	with bench.mark("cuts reformat"):
 		ocuts = cuts.to_simple()
 
@@ -581,6 +582,7 @@ def calibrate(data, meta, mul=32, dev=None, prev_obs=None):
 	res  = bunch.Bunch()
 	res.dets         = meta.aman.dets.vals[good]
 	res.detids       = meta.aman.det_ids[good]
+	res.detpix       = meta.aman.det_pix[good]
 	res.bands        = meta.aman.bands[good]
 	res.point_offset = meta.aman.focal_plane[good,1::-1]
 	res.polangle     = meta.aman.focal_plane[good,2]
@@ -591,7 +593,10 @@ def calibrate(data, meta, mul=32, dev=None, prev_obs=None):
 	res.cuts         = ocuts
 	res.site         = "so"
 	res.response     = None
-	res.overpole     = overpole # whether el was originally > 90°. TODO: update the other loaders
+	# original value of the first sample of boresight, before the pointing model
+	# preserves the original winding, e.g.. el > 90, az > 360, etc, so can be used
+	# to restore this information where it is necessary
+	res.bore_ref     = bore_ref # [el0,az0,roll0]
 	# Test per-detector response
 	#res.response     = dev.np.zeros((2,len(res.tod)),res.tod.dtype)
 	#res.response[0]  = 2
@@ -665,6 +670,10 @@ class WaferInfo:
 				dets  = np.char.decode(data["dets:det_id"]),
 				bands = np.char.decode(data["dets:wafer.bandpass"]),
 				types = np.char.decode(data["dets:wafer.type"]),
+				array = np.char.decode(data["dets:wafer.array"]),
+				rhombus = np.char.decode(data["dets:wafer.rhombus"]),
+				row   = data["dets:wafer.det_row"],
+				col   = data["dets:wafer.det_col"],
 				raw   = data)
 
 # Should extend DetCache to also include wafer_info. wafer_info
@@ -729,20 +738,30 @@ class DetCache:
 			ainds, winds = utils.common_inds([ainfo.dets, winfo.dets])
 			# Group by band:type
 			band_type = np.char.add(np.char.add(winfo.bands[winds],":"),winfo.types[winds])
+			# Build a pixel id. This is the same for all detectors that are co-located
+			# in the focal plane, and is usedful for pair differencing etc
+			pixid = build_pixid(winfo.array[winds], winfo.rhombus[winds], winfo.row[winds], winfo.col[winds])
 			bts, order, edges = utils.find_equal_groups_fast(band_type)
 			for bti, bt in enumerate(bts):
 				band, type = bt.split(":")
 				subid = "%s:%s:%s:%s" % (obsid, sinfo.wslot, band, type)
-				inds  = ainds[order[edges[bti]:edges[bti+1]]]
+				ginds = order[edges[bti]:edges[bti+1]]
+				inds  = ainds[ginds]
 				self.det_cache[subid] = bunch.Bunch(
 						channels = ainfo.channels[inds],
 						dets     = ainfo.dets[inds],
 						detset   = dset,
 						band     = band,
 						type     = type,
+						pix      = pixid[ginds],
 				)
 			self.detset_cache[obsid][sinfo.wslot] = dset
 		self.done.add(obsid)
+
+def build_pixid(array, rhombus, row, col):
+	# Annoying that numpy.char doesn't provide any real vectorized
+	# operations for this, only pretend ones
+	return np.array(["%s_%sr%02dc%02d" % (a,R,r,c) for a,R,r,c in zip(array, rhombus, row, col)])
 
 class Subid:
 	def __init__(self, obsid, wslot, band, type="OPTC"):

python/nmat.py
@@ -36,6 +36,7 @@ class NmatNull(Nmat):
 	def __init__(self, dev=None):
 		self.dev   = dev or device.get_device()
 		self.ready = True
+		self.ivar  = np.zeros(10000,np.float32)
 	def build(self, tod, srate, **kwargs): return self
 	def apply(self, tod, inplace=True):
 		if not inplace: tod = tod.copy()
@@ -58,9 +59,18 @@ class NmatDebug(Nmat):
 	def build(self, tod, srate, **kwargs):
 		nsamp  = tod.shape[1]
 		nblock = nsamp//self.bsize
-		var    = self.dev.np.median(self.dev.np.var(tod[:,:nblock*self.bsize].reshape(-1,nblock,self.bsize),-1),-1)
+		vars   = self.dev.np.var(tod[:,:nblock*self.bsize].reshape(-1,nblock,self.bsize),-1)
+		# Robustness for cases with huge gapfilled regions
+		fallback = vars*0 + self.dev.np.var(tod,-1)[:,None]
+		bad    = vars==0
+		vars[bad] = fallback[bad]
+		var    = self.dev.np.median(vars,-1)
 		with utils.nowarn():
 			ivar = utils.without_nan(1/var)*self.downweight
+		# More robustness
+		ref    = self.dev.np.median(ivar)
+		if ref == 0: ivar[:] = 0
+		else: ivar[ivar>ref*100] = 0
 		f = self.dev.np.fft.rfftfreq(nsamp, 1/srate)
 		with utils.nowarn():
 			profile = 1/(1+(f/self.fknee)**self.alpha)
@@ -225,7 +235,8 @@ class NmatDetvecs(Nmat):
 		tod   = self.dev.np.asarray(tod)
 		gutils.apply_window(tod, nwin)
 		#bunch.write("test_sogma_full.hdf", bunch.Bunch(tod=tod, srate=srate))
-		ft    = self.dev.lib.rfft(tod)
+		ft    = self.dev.pools["ft"].empty((ndet,nfreq), utils.complex_dtype(dtype))
+		self.dev.lib.rfft(tod, ft)
 		# Unapply window again
 		gutils.apply_window(tod, nwin, -1)
 		#del tod
@@ -362,10 +373,10 @@ class NmatDetvecsScaled(Nmat):
 		nwin  = utils.nint(self.window*srate)
 		ndet, nsamp = tod.shape
 		nfreq = nsamp//2+1
-		tod   = self.dev.np.asarray(tod)
 		gutils.apply_window(tod, nwin)
 		#bunch.write("test_sogma_full.hdf", bunch.Bunch(tod=tod, srate=srate))
-		ft    = self.dev.lib.rfft(tod)
+		ft    = self.dev.pools["ft"].empty((ndet,nfreq), utils.complex_dtype(dtype))
+		self.dev.lib.rfft(tod, ft)
 		# Unapply window again
 		gutils.apply_window(tod, nwin, -1)
 
@@ -473,7 +484,124 @@ class NmatDetvecsScaled(Nmat):
 				window=data.window, nwin=data.nwin, sampvar=data.sampvar, bsize_mean=data.bsize_mean, himps=data.himps,
 				bins=data.bins, D=data.D, V=data.V, E=data.E, ivar=data.ivar, dev=dev)
 
+class NmatPairRot(Nmat):
+	""""Rotate" from normal tod to pair-diffs and pair-sums before constructing
+	another noise model."""
+	def __init__(self, nmat, frange=[0.01,0.1], wrange=[10,20], rot=None, ivar=None, dev=None):
+		if dev is None: dev = device.get_device()
+		self.dev     = dev
+		self.nmat    = nmat
+		self.frange  = np.array(frange)
+		self.wrange  = np.array(wrange)
+		self.rot     = rot
+		self.ivar    = ivar
+		self.ready   = nmat.ready and rot is not None and ivar is not None
+	def build(self, tod, srate, obs, extra=False, **kwargs):
+		# Find pairs and singles. This will throw an error if
+		# we get triples or higher, which this class doesn't
+		# know how to deal with
+		singles, pairs = gutils.find_det_tuples(obs, nmin=2, transpose=True)
+		# Will determine our pair scaling using a small range in fourier spac3
+		df   = srate/tod.shape[1]
+		ifrange = utils.ceil(self.frange/df)
+		iwrange = utils.ceil(self.wrange/df)
+		with self.dev.pools["ft"].as_allocator():
+			ft = self.dev.lib.rfft(tod)
+		# Estimate unrotated white noise level, which we need for our .ivar member
+		ivar = tod.shape[1]/self.dev.np.mean(np.abs(ft[:,iwrange[0]:iwrange[1]])**2,-1)
+		pft  = ft[pairs,ifrange[0]:ifrange[1]] # [2,npair,:]
+		pft  = pft.view(utils.real_dtype(pft.dtype))
+		# Find what pft[1] must be multiplied by to minimize its diff with pft[0]
+		amps = self.dev.np.sum(pft[0]*pft[1],-1)/self.dev.np.sum(pft[1]*pft[1],-1)
+		# Apply our rotation so we can forward to our inner model. Would
+		# normally overwrite tod, but will need it after this. Could unrotate, but
+		# that could lose some precision, so we make a copy instead
+		rot  = PairRot(singles, pairs, amps, pool=self.dev.pools["ft"], dev=self.dev)
+		wtod = self.dev.pools["pointing"].array(tod)
+		rot.apply(wtod)
+		# We forward obs, but beware that the det order is messed up!
+		nmat = self.nmat.build(wtod, srate, obs=obs, extra=extra, **kwargs)
+		return NmatPairRot(nmat, frange=self.frange, rot=rot, ivar=ivar, dev=self.dev)
+	def apply(self, tod, inplace=True):
+		# Have Q = cov(Rd) = R<dd'>R' = RNR' => N = R"'Q"R" => N" = RQR'
+		# R  = rotate*scale*reorder
+		# R' = reorder'*scale'*rotate'
+		# rotate   = [[1,1],[1,-1]] = rotate'
+		# reorder' = reorder"
+		# scale'   = scale
+		# Hence, R' = R", except the scaling isn't inverted
+		self.check_ready()
+		t1 = self.dev.time()
+		if not inplace: tod = tod.copy()
+		self.rot.apply(tod)
+		t2 = self.dev.time()
+		self.nmat.apply(tod)
+		t3 = self.dev.time()
+		self.rot.adjoint(tod)
+		t4 = self.dev.time()
+		L.print("iN sub rot %6.4f child %6.4f rot' %6.4f ndet %3d nsamp %5d" % (t2-t1,t3-t2,t4-t3,tod.shape[0],tod.shape[1]), level=3)
+		return tod
+	#def white(self, tod, inplace=True, nwin=None):
+	#	# Consider just using ivar here...
+	#	self.check_ready()
+	#	if not inplace: gtod.copy()
+	#	self.rot.apply(tod)
+	#	self.nmat.white(tod, nwin=nwin)
+	#	self.rot.adjoint(tod)
+	#	return tod
+	def white(self, tod, inplace=True, nwin=None):
+		self.check_ready()
+		if not inplace: tod.copy()
+		if nwin is None: nwin = self.nmat.nwin
+		gutils.apply_window(tod, nwin)
+		tod *= self.ivar[:,None]
+		gutils.apply_window(tod, nwin)
+		return tod
+	def write(self, fname): raise NotImplementedError
+	@staticmethod
+	def from_bunch(data, dev=None): raise NotImplementedError
 
+class PairRot:
+	def __init__(self, singles, pairs, amps, pool=None, dev=None):
+		if dev  is None: dev  = device.get_device()
+		if pool is None: pool = self.dev.np
+		self.singles = singles
+		self.pairs   = pairs
+		self.amps    = amps
+		self.pool    = pool
+		self.dev     = dev
+	@property
+	def ns(self): return self.singles.shape[1]
+	def apply(self, tod, work=None):
+		if work is None: work = self.pool.empty(tod.shape, tod.dtype)
+		sshape = self.singles.shape+tod.shape[-1:]
+		pshape = self.pairs  .shape+tod.shape[-1:]
+		# Extract into contiguous blocks
+		wstod = self.dev.np.take(tod, self.singles, axis=0, out=work[:self.ns].reshape(sshape))
+		wptod = self.dev.np.take(tod, self.pairs,   axis=0, out=work[self.ns:].reshape(pshape))
+		# Apply scaling
+		wptod[1] *= self.amps[:,None]
+		# Apply the "rotation" into tod
+		gutils.pair_rotate(wptod, tod[self.ns:].reshape(pshape))
+		tod[:self.ns] = wstod[0]
+		return tod
+	def adjoint(self, tod, work=None):
+		return self.unapply(tod, work=work, adjoint=True)
+	def unapply(self, tod, work=None, adjoint=False):
+		if work is None: work = self.pool.empty(tod.shape, tod.dtype)
+		pshape = self.pairs  .shape+tod.shape[-1:]
+		# Unapply the rotation into work
+		wptod = work[self.ns:].reshape(pshape)
+		gutils.pair_rotate(tod[self.ns:].reshape(pshape), wptod)
+		# Copy over singles too
+		work[:self.ns] = tod[:self.ns]
+		# Unapply scaling
+		if adjoint: wptod[1] *= self.amps[:,None]
+		else:       wptod[1] /= self.amps[:,None]
+		# Insert at original positions
+		tod[self.singles.reshape(-1)] = work[:self.ns]
+		tod[self.pairs  .reshape(-1)] = work[self.ns:]
+		return tod
 
 # Originally considered a model of this form,
 #  ps = ps_detcov * Δperdet * Δmean * Δtweak
@@ -807,27 +935,24 @@ class NmatAdaptive(Nmat):
 				b["%s%02d" % (name,i)] = self.dev.get(v)
 		bunch.write(fname, b)
 
-class PseudoNmatGapfill(Nmat):
-	def __init__(self, mask, sys="equ,on=saturn", bsize=256, ivar=None, cuts=None, srate=None, buf="model", dev=None):
+class Filter:
+	def __init__(self, dev=None):
+		"""Initialize the filter. In subclasses this will typically set up parameters"""
+		self.dev   = dev or device.get_device()
+	def apply(self, tod, srate=1, obs=None):
+		"""Apply the filter to tod, overwriting it with the result"""
+		pass
+
+class FilterNone(Filter): pass
+
+class FilterGapfill(Filter):
+	def __init__(self, mask, sys="equ,on=saturn", buf="model", dev=None):
 		self.dev   = dev or device.get_device()
 		assert mask.ndim == 3 and mask.shape[0] == 3 and mask.dtype == np.float32
 		self.mask  = mask
 		self.sys   = sys
-		self.bsize = bsize
 		self.buf   = buf
-		self.nwin  = 0
-		self.srate = srate
-		self.ivar  = None if ivar is None else self.dev.np.asarray(ivar)
-		self.cuts  = cuts
-	@property
-	def ready(self): return self.ivar is not None and self.cuts is not None and self.srate is not None
-	def build(self, tod, srate, obs, **kwargs):
-		# First measure the white noise level
-		nsamp  = tod.shape[1]
-		nblock = nsamp//self.bsize
-		var    = self.dev.np.median(self.dev.np.var(tod[:,:nblock*self.bsize].reshape(-1,nblock,self.bsize),-1),-1)
-		with utils.nowarn():
-			ivar = utils.without_nan(1/var)
+	def apply(self, tod, srate, obs, **kwargs):
 		# Then build our cut object. Very similar to socal.object_cut, but it didn't quite
 		# match what I wanted here. It didn't allow one to specify a coordinate system
 		from . import pmat, tiling
@@ -839,30 +964,11 @@ class PseudoNmatGapfill(Nmat):
 		# turn non-zero values into a cuts object
 		from . import socal
 		cuts = socal.mask2cut(wtod, dev=self.dev)
-		return PseudoNmatGapfill(mask=self.mask, sys=self.sys, bsize=self.bsize,
-			ivar=ivar, cuts=cuts, srate=srate, buf=self.buf, dev=self.dev)
-	def apply(self, tod, inplace=True):
-		"""Doesn't actually act like multiplying by a noise matrix.
-		Instead it uses the areas outside the mask to predict and subtract the
-		correlated noise inside the mask"""
-		self.check_ready()
-		if not inplace: tod.copy()
 		# Build our gapfilled model
 		model = self.dev.pools[self.buf].array(tod)
-		gutils.estimate_atm_tod(model, self.cuts, srate=self.srate, dev=self.dev, pool=self.dev.pools["ft"])
+		gutils.estimate_atm_tod(model, cuts, srate=srate, dev=self.dev, pool=self.dev.pools["ft"])
 		# subtract prediction from tod to clean the atmosphere
 		tod -= model
-		return tod
-	def white(self, tod, inplace=True, nwin=None):
-		# This is the only part of this "Nmat" that actually acts as a noise matrix
-		self.check_ready()
-		if not inplace: gtod.copy()
-		gtod *= self.ivar[:,None]
-		return gtod
-	def write(self, fname): return NotImplementedError
-	@staticmethod
-	def from_bunch(data, dev=None): return NotImplementedError
-
 
 #################
 # Helpers below #
@@ -1095,7 +1201,6 @@ def noise_modes_hybrid(ft, bins, weight=None, mask=None, eig_lim=16, single_lim=
 	#    narrow bins.
 	if weight is not None: ft *= weight # avoids copy, at cost of weight=0 breaking
 	V     = find_modes_merged(ft, bins, eig_lim=eig_lim, single_lim=single_lim).astype(wtype, copy=False)
-	print("V.shape", V.shape)
 	if weight is not None: ft /= weight
 	nglob = V.shape[1]
 	nmax  = min(nmax, ndet//2)

python/tiling.py
@@ -221,6 +221,30 @@ class TileDistribution:
 		if buf: arr = buf.zeros(self.work.shape, dtype, reset=reset)
 		else:   arr = self.dev.np.zeros(self.work.shape, dtype)
 		return self.dev.lib.LocalMap(self.work.lp, arr)
+	# How to get the coordinates of each cell we own. Sadly cumbersome because
+	# of the messy structure in this class.
+	# 1. shape, wcs is the fullsky geometry that we're tiling
+	# 2. downer[ty,tx] is the owner of fullsky[ty*tsize:(ty+1)*tsize,tx*tsize:(tx+1)*tsize]
+	# 3. corners = np.array(np.where(downer==comm.rank)).T*tsize are the corner coordinates
+	#    of each of the tiles we own
+	# 4. We assume car, so given tpix = np.mgrid[:tsize,:tsize], the coordinate of each
+	#    tile-pixel will be tpix*wcs.wcs.cdelt[1::-1,None,None]
+	def dpixmap(self):
+		"""Return the global pixel-coordinates of each pixel in our dmap
+		as pixmap[{y,x},ntile,ny,nx]"""
+		corners = np.array(np.where(self.dist.owner==self.comm.rank))*self.tsize
+		relpix  = np.mgrid[:self.tshape,:self.tshape]
+		return corners[:,:,None,None]+relpix
+	def dposmap(self):
+		"""Returns the global sky-coordinates of each pixel in our dmap
+		as posmap[{dec,ra},ntile,ny,nx].
+		Assumes CAR! Could easily be generalized"""
+		assert self.wcs.wcs.ctype[0][-3:] == "CAR"
+		posmap  = self.dpixmap().astype(float)
+		posmap -= self.wcs.wcs.crpix[1::-1,None,None,None]
+		posmap *= cdelt[1::-1,None,None,None]*utils.degree
+		posmap += self.wcs.wcs.crval[1::-1,None,None,None]*utils.degree
+		return posmap
 
 def build_dist_tiling(dist_owner, comm, tsize=1):
 	dist = bunch.Bunch(owner=dist_owner)
@@ -418,22 +442,43 @@ def distribute_global_tiles_exposed_simple(loc_cells, comm):
 	owner[mask] = np.arange(nhit)*comm.size//nhit
 	return owner
 
-def distribute_tods_simple(obsinfo, nsplit):
+def get_weight_detsamps(obsinfo, joint=None):
+	if joint is None: return obsinfo.ndet.astype(int)*obsinfo.nsamp
+	ntot = []
+	for g in joint.groups:
+		ndet  = np.sum(obsinfo.ndet [g])
+		nsamp = np.sum(obsinfo.nsamp[g])
+		# int to get 8-byte, to avoid overflow
+		ntot.append(int(ndet)*nsamp)
+	return np.array(ntot)
+
+def distribute_tods_simple(obsinfo, nsplit, joint=None):
 	return bunch.Bunch(
 		owner   = np.arange(len(obsinfo))%nsplit,
 		weights = np.ones(nsplit),
 	)
 
-def distribute_tods_ra(obsinfo, nsplit, verbose=False):
+def distribute_tods_ra_plain(obsinfo, nsplit, joint=None, verbose=False):
+	"""Split tods by the typical ra value without weighting."""
+	ra     = np.mean(obsinfo.sweep[:,:,0],-1)
+	order  = np.argsort(ra)
+	owner  = np.zeros(len(obsinfo),int)
+	owner[order] = np.arange(len(obsinfo))*nsplit//len(obsinfo)
+	return bunch.Bunch(
+		owner   = owner,
+		weights = np.ones(nsplit),
+	)
+
+def distribute_tods_ra(obsinfo, nsplit, weight, verbose=False):
 	"""Split tods by the typical ra value. This isn't optimal, but it's
 	fast and robust, unlike distribute_tods_semibrute, that sometimes fails
-	and leaves things very unbalanced."""
+	and leaves things very unbalanced. This version tries to keep the total
+	number of det-samples per split the same."""
 	ntod  = len(obsinfo)
 	# Handle special cases
 	if nsplit == 1: return bunch.Bunch(owner=np.zeros(ntod,int), weight=np.ones(ntod))
 	if ntod <= nsplit: return bunch.Bunch(owner=np.arange(ntod), weight=np.ones(ntod))
 	ra     = np.mean(obsinfo.sweep[:,:,0],-1)
-	weight = obsinfo.ndet*obsinfo.nsamp
 	# Hand out blocks in ra-sorted space
 	order  = np.argsort(ra)
 	cweight= np.cumsum(weight[order])
@@ -447,7 +492,7 @@ def distribute_tods_ra(obsinfo, nsplit, verbose=False):
 	wper   = np.bincount(owner, weight, minlength=nsplit)
 	return bunch.Bunch(owner=owner, weights=wper)
 
-def distribute_tods_semibrute(obsinfo, nsplit, npass=2, niter=10, verbose=False):
+def distribute_tods_semibrute(obsinfo, nsplit, weight, npass=2, niter=10, verbose=False):
 	"""Split into nsplit groups such that the groups are
 	are approximately maximally-separated in state-space,
 	and each group has approximately the same weight.
@@ -466,7 +511,6 @@ def distribute_tods_semibrute(obsinfo, nsplit, npass=2, niter=10, verbose=False)
 	pos   = np.array([obsinfo.sweep[:,:,1],obsinfo.sweep[:,:,0]]) # [{y,x},ntod,npoint]
 	state = np.moveaxis(pos,1,0).reshape(ntod,-1) # [ntod,ncoord]
 	# TODO: Do I need to unwind state here?
-	weight= obsinfo.ndet*obsinfo.nsamp
 	# Start from the point with the lowest state-sub
 	refs, dists  = find_refs_rimwise(state, nsplit)
 	gid, gweight = assign_to_group_semibrute(dists, weight, nsplit, niter=niter, state=state, verbose=verbose)